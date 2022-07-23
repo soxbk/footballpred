4abf81a0c5969817d5ebf927b301a1a926b5c410
soxbk <48887825+soxbk@users.noreply.github.com>
2022-07-22 22:00:30 -0700

    minor

--- myFootballSite/settings.py
+++ myFootballSite/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5f^91amvuap3$t$)-p0iz#cwhbz%gv7xm^_jj63u*)306)w93&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = []
 
