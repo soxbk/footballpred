82f0699436bb33c724dbbcaac6156919ff0f7c18
soxbk <48887825+soxbk@users.noreply.github.com>
2022-07-22 22:13:27 -0700

    minor

--- myFootballSite/settings.py
+++ myFootballSite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5f^91amvuap3$t$)-p0iz#cwhbz%gv7xm^_jj63u*)306)w93&
 DEBUG = False
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =['footballpredictor22.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
